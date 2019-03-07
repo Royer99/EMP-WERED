8694f447852ff39b7592dd551781f1620120bed5
Ivan Ramirez <Ivanramirez@Ivan-Ramirez-2.local>
2019-03-06 19:53:43 -0600

    modified:   src/characters/enemy/Bully.java 	modified:   src/characters/enemy/Enemy.java

---
+++ src/characters/enemy/Enemy.java
@@ -0,0 +1,14 @@
+package characters;
+
+public class abstract Enemy extends Character
+        {
+
+            public abstract void mover()
+            {
+
+            }
+            public abstract void paint();
+            public abstract void ataca();
+
+
+        }
